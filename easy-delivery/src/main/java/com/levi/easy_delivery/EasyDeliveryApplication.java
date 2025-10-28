5640dacac382c2f763abd202d6f4a45ed9de8c7c
levibezerra <levilimabezerra20@gmail.com>
2025-10-28 19:39:18 -0300

    Adicionando projeto e documentação

---
+++ easy-delivery/src/main/java/com/levi/easy_delivery/EasyDeliveryApplication.java
@@ -0,0 +1,12 @@
+package com.levi.easy_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDeliveryApplication.class, args);
+	}
+}
---
+++ easy-delivery/src/test/java/com/levi/easy_delivery/EasyDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi.easy_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
