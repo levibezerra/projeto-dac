471f6817d5c7cd9b8a0d642e7cab03224de8a484
levibezerra <levilimabezerra20@gmail.com>
2025-10-28 21:07:09 -0300

    Adicionando a classe Usuario e o enum Role

---
+++ easy-delivery/src/main/java/com/levi/easy_delivery/entity/Usuario.java
@@ -0,0 +1,63 @@
+package com.levi.easy_delivery.entity;
+
+import com.levi.easy_delivery.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "senha", nullable = false, length = 200)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "data_de_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_de_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ easy-delivery/src/main/java/com/levi/easy_delivery/enums/Role.java
@@ -0,0 +1,6 @@
+package com.levi.easy_delivery.enums;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_CLIENTE
+}
