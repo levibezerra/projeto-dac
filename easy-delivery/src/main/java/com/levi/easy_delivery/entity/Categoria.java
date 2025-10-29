6ca3161984234d3232b3046f9507c89e87ad50ec
levibezerra <levilimabezerra20@gmail.com>
2025-10-28 22:05:53 -0300

    Adicionando a classe Categoria

--- easy-delivery/src/main/java/com/levi/easy_delivery/entity/Categoria.java
+++ easy-delivery/src/main/java/com/levi/easy_delivery/entity/Categoria.java
@@ -1,48 +1,27 @@
 package com.levi.easy_delivery.entity;
 
-import com.levi.easy_delivery.enums.StatusDoPrato;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Getter @Setter @AllArgsConstructor @NoArgsConstructor
-
 @Entity
-@Table(name = "pratos")
-public class Prato {
+@Table(name = "categorias")
+public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome", nullable = false, length = 100)
+    @Column(name = "nome_da_categoria", nullable = false, length = 100)
     private String nome;
 
-    @Column(name = "descricao", nullable = false, length = 100)
+    @Column(name = "descricao_da_categoria", nullable = false, length = 100)
     private String descricao;
-
-    @Column(name = "preco", nullable = false, columnDefinition = "decimal(7,2)")
-    private BigDecimal preco;
-
-    @Column(name = "imagem_do_prato")
-    private String imagemUrl;
-
-    @Column(name = "status_do_prato", nullable = false)
-    private StatusDoPrato status;
-
-    @ManyToOne
-    @JoinColumn(name = "categoria_id", nullable = false)
-    private Categoria categoria;
 
     @CreatedDate
     @Column(name = "data_de_criacao")
@@ -63,8 +42,8 @@ public class Prato {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        Prato prato = (Prato) o;
-        return Objects.equals(id, prato.id);
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
     }
 
     @Override
