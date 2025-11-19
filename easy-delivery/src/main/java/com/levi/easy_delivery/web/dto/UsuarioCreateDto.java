a66c6830e8715a523b0049c22467b11e6f1a9d23
levibezerra <levilimabezerra20@gmail.com>
2025-11-19 12:13:26 -0300

    update(atualizar a classe UsuarioCreateDto e o método create em UsuarioController)

--- easy-delivery/src/main/java/com/levi/easy_delivery/web/dto/UsuarioCreateDto.java
+++ easy-delivery/src/main/java/com/levi/easy_delivery/web/dto/UsuarioCreateDto.java
@@ -1,11 +1,18 @@
 package com.levi.easy_delivery.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
 
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "formato de e-mail invalido!", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String email;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String senha;
 }
